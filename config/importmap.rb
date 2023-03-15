d8b26680e0aaa9a15310274facbcc8ea1ca836ca
Matt Hosier <mrhosier42@gmail.com>
2023-03-15 00:58:25 -0500

    fixing things

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,8 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+# Newly added - Matt Hosier
+pin "bootstrap", to: "bootstrap/dist/css/bootstrap.min.css"
+pin "bootstrap.bundle", to: "bootstrap/dist/js/bootstrap.bundle.min.js"
+# end here
 pin_all_from "app/javascript/controllers", under: "controllers"
