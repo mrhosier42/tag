270b1861dd088efe44a51e4054238d1bee96b236
Matt Hosier <mrhosier42@gmail.com>
2023-03-21 22:12:33 -0500

    Reverting last commit changes, reason: In Rails 7, it wants to rely less on javascript tool and be more lightweight. Turbo and hotwire to do what JavaScript did.
    More on this: https://blog.saeloun.com/2021/11/16/replacing-webpacker-with-importmapped

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 //= require survey_page/survey_page_expanding_cols
-//= require jquery
-//= require jquery_ujs
 import "@hotwired/turbo-rails";
 import "controllers";
