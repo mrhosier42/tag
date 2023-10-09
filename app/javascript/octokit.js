a9c52ca0cebf286233d89e36f60801ce54946bb3
Joshua Clark <joshuaclark2014@email.com>
2023-10-09 17:19:52 -0500

    Branch for github integration

---
+++ app/javascript/octokit.js
@@ -0,0 +1,34 @@
+const { Octokit } = require("@octokit/rest");
+
+console.log("Javascript File is executed.");
+
+const octokit = new Octokit({
+    auth: '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',
+  });
+  
+// Get a reference to the commit-list element
+const commitList = document.getElementById('commit-list');
+
+octokit.rest.repos.listCommits({
+owner: 'mrhosier42',
+repo: 'tag',
+})
+
+.then((response) => {
+// Handle the API response here
+const commits = response.data;
+
+// Create an HTML string to display the commits
+const commitsHTML = commits.map((commit) => {
+    return `<p><strong>${commit.commit.author.name}</strong>: ${commit.commit.message}</p>`;
+}).join('');
+
+// Update the content of the commit-list element
+commitList.innerHTML = commitsHTML;
+})
+
+.catch((error) => {
+// Handle any errors here
+console.error(error);
+});
+
